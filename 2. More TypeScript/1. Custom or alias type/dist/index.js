ec25e9b3f3944f7aa9a38405e51cf122cad42074
FarhanTips <farhanakbor02@gmail.com>
2026-08-16 18:17:46 +0600

    Custom types or alias

---
+++ 2. More TypeScript/1. Custom or alias type/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const robin = {
+    name: "Robin",
+    roll: 1,
+    major: "CSE",
+    gpa: 3.8
+};
+//# sourceMappingURL=index.js.map
---
+++ 2. More TypeScript/1. Custom or alias type/dist/type/array of objects.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=array%20of%20objects.d.ts.map
