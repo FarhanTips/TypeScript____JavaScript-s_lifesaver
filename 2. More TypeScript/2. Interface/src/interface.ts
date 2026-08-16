ce433791b4574d0471c8c4bcf6b21e04bd618600
FarhanTips <farhanakbor02@gmail.com>
2026-08-16 21:00:51 +0600

    Interface Intro

---
+++ 2. More TypeScript/2. Interface/src/interface.ts
@@ -0,0 +1,71 @@
+
+
+// object-এর জন্য interface ব্যবহার করা ভালো।
+
+interface Employee {
+    name: string,
+    id: number,
+    department : string,
+    salary?: number
+}
+
+const elon: Employee = {
+    name: "Elon",
+    id: 101,
+    department: "HR",
+    salary: 20000
+}
+
+
+const bill: Employee = {
+    name: "Bill",
+    id: 1012,
+    department: "CEO"
+}
+
+
+
+const team: Employee[] = [elon, bill, {
+    name:"Farhan",
+    id: 12,
+    department: "Software",
+    salary: 12000050
+}];
+
+
+
+
+function printEmploueeDetails(employee : Employee): void{
+    console.log(employee.name);
+    console.log(employee.id);
+    console.log(employee.department);
+}
+
+printEmploueeDetails(elon);
+printEmploueeDetails({name:"Montu", id: 3, department:"Search"});
+
+
+
+// destructuring directly in the parameter
+function displayEmploueeDetails({name, department} : Employee): void{
+    console.log(name);
+    console.log(department);
+}
+
+displayEmploueeDetails(bill);
+displayEmploueeDetails({name:"Banti", id: 37, department:"Supply Chain"});
+
+// So an Employee object must have:
+
+// name
+// id
+// department
+// salary is optional
+
+// destructure = From the Employee object, I only want to extract name and department. It doesn't mean that id is no longer required.
+
+// Even though you don't use id inside the displayEmploueeDetails() function, TypeScript still requires the id property because the parameter is typed as Employee.
+
+
+// We can think like Give me an Employee, but inside this function I only need its name and department.
+
