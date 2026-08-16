70231f58e7aebee7a7454752e60ca543eb4fcecc
FarhanTips <farhanakbor02@gmail.com>
2026-08-16 18:20:50 +0600

    Custom types or alias

---
+++ 2. More TypeScript/1. Custom or alias type/src/index.ts
@@ -0,0 +1,14 @@
+
+
+const robin : {
+    name: string,
+    roll: number,
+    major: string,
+    gpa: number
+} = {
+    name: "Robin",
+    roll: 1,
+    major: "CSE",
+    gpa: 3.8
+};
+
---
+++ 2. More TypeScript/1. Custom or alias type/src/type/type.ts
@@ -0,0 +1,43 @@
+
+
+type Student = {
+    name: string,
+    roll: number,
+    major: string,
+    gpa?: number
+}
+
+
+const robin: Student = {
+    name: "Robin",
+    roll: 1,
+    major: "CSE",
+    gpa: 3.8
+};
+
+
+
+const mobin: Student = {
+    name: "Robin",
+    roll: 1,
+    major: "CSE",
+    gpa: 3.8
+};
+
+
+
+const lobin: Student = {
+    name: "Robin",
+    roll: 1,
+    major: "CSE",
+    gpa: 3.8
+};
+
+
+
+const jobin: Student = {
+    name: "Robin",
+    roll: 1,
+    major: "CSE",
+};
+
