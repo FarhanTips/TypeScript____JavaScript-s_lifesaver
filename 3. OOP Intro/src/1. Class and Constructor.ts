db7a6b33429a68756e8ce944bae0d9418760e8a1
FarhanTips <farhanakbor02@gmail.com>
2026-08-18 20:43:00 +0600

    OOP Basic

---
+++ 3. OOP Intro/src/1. Class and Constructor.ts
@@ -0,0 +1,33 @@
+
+
+
+/**
+ * name
+ * email
+ * age 
+ * marks
+ */
+
+class Student {
+    name: string;
+    email: string;
+    age: number;
+    marks: number;
+
+    // Constructor → special method
+    constructor(name: string, email: string, age: number, marks: number) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.marks = marks;
+    }
+}
+
+// Instantiate
+// Instance
+const rafi = new Student("Rafi", "rafi@gmail.com", 16, 100);
+const sadia = new Student("Sadia", "sadia@gmail.com", 17, 200);
+
+console.log(sadia);
+
+
