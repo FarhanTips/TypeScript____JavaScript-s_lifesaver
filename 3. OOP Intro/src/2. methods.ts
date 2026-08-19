3d286efb5c19d12f3236fc9d53400a4feb5cfb34
FarhanTips <farhanakbor02@gmail.com>
2026-08-19 11:54:59 +0600

    methods updated

--- 3. OOP Intro/src/2. methods.ts
+++ 3. OOP Intro/src/2. methods.ts
@@ -8,32 +8,33 @@
  */
 
 class Student {
-    name:string
-    email:string
-    age:number
-    marks:number
+    name: string;
+    email: string;
+    age: number;
+    marks: number;
 
     //constructor ---> special method
-    constructor(name:string, email:string, age:number, marks:number) {
-        this.name = name
-        this.email = email
-        this.age = age
-        this.marks = marks
+    constructor(name: string, email: string, age: number, marks: number) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.marks = marks;
     }
 
-    getInfo():string {
-        const info = `Name: ${this.name}\nEmail: ${this.email}`
-        return info
+    getInfo(): string {
+        const info = `Name: ${this.name}\nEmail: ${this.email}`;
+        return info;
     }
-
 }
 
 //instantiate
 //instance
-// const rafi = new Student("Rafi", "rafi@gmail.com", 16, 100)
-// const sadia = new Student("Sadia", "sadia@gmail.com", 17, 200)
+// const rafi = new Student("Rafi", "rafi@gmail.com", 16, 100);
+// const sadia = new Student("Sadia", "sadia@gmail.com", 17, 200);
 
-// console.log(sadia)
+// console.log(sadia);
+
+
 
 
 /**
@@ -41,38 +42,36 @@ class Student {
  * soldItems
  */
 
-class ChaShop{
-    title:string
-    soldItems:any = []    // you can declare type AND value. You don't have to write only the type.
+class ChaShop {
+    title: string;
+    soldItems: any = [];    // you can declare type AND value. You don't have to write only the type.
 
 
-    constructor(title:string) {
-        this.title = title
+    constructor(title: string) {
+        this.title = title;
     }
 
 
-    buy(name:string, price:number) {
-      this.soldItems.push({name, price})
+    buy(name: string, price: number) {
+        this.soldItems.push({ name, price });
     }
 
     totalSold() {
-        const total = this.soldItems.reduce((acc, item) => acc+item.price, 0)
-        return total
+        const total = this.soldItems.reduce((acc, item) => acc + item.price, 0);
+        return total;
     }
-
-
 }
 
 
-const shop1 = new ChaShop("Tea Shop")
-const shop2 = new ChaShop("Cofffe Shop")
+const shop1 = new ChaShop("Tea Shop");
+const shop2 = new ChaShop("Cofffe Shop");
 
-shop1.buy("Tea", 10)
-shop1.buy("Cake", 15)
-shop2.buy("Coffee", 35)
+shop1.buy("Tea", 10);
+shop1.buy("Cake", 15);
+shop2.buy("Coffee", 35);
 
-console.log(shop1.totalSold())
-console.log(shop2.totalSold())
+console.log(shop1.totalSold());
+console.log(shop2.totalSold());
 
 
 
