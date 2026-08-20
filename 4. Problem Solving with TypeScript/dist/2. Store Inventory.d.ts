0db6b88f7e1daa140b976167a2c1c89f62a8d78a
FarhanTips <farhanakbor02@gmail.com>
2026-08-20 16:17:10 +0600

    Store Inventory Status

---
+++ 4. Problem Solving with TypeScript/dist/2. Store Inventory.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=2.%20Store%20Inventory.d.ts.map
---
+++ 4. Problem Solving with TypeScript/src/2. Store Inventory.ts
@@ -0,0 +1,33 @@
+
+
+
+function getStockStatus(stock:number): string{
+    if (stock === 0){
+        return "Out of Stock";
+    }
+    else if (stock < 6){
+        return "Almost Sold Out";
+    }
+    else if (stock < 21){
+        return "Available";
+    }
+    return "In Stock";
+}
+
+console.log(getStockStatus(0));
+// "Out of Stock"
+ 
+console.log(getStockStatus(3));
+// "Almost Sold Out"
+ 
+console.log(getStockStatus(12));
+// "Available"
+ 
+console.log(getStockStatus(50));
+// "In Stock"
+ 
+// boundary values to double-check:
+console.log(getStockStatus(5));
+console.log(getStockStatus(6));
+console.log(getStockStatus(20));
+console.log(getStockStatus(21));
