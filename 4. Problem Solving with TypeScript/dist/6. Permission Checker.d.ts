422a5b6039a3dfb53d500920e85c9cc2fbd467ce
FarhanTips <farhanakbor02@gmail.com>
2026-08-21 15:40:14 +0600

    Role-Based Permission Checker

---
+++ 4. Problem Solving with TypeScript/dist/6. Permission Checker.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=6.%20Permission%20Checker.d.ts.map
---
+++ 4. Problem Solving with TypeScript/dist/6. Permission Checker.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function canEdit(role) {
+    return role == "admin" || role == "editor" ? true : false;
+}
+console.log(canEdit("admin"));
+// true 
+console.log(canEdit("editor"));
+// true 
+console.log(canEdit("viewer"));
+// false 
+//# sourceMappingURL=6.%20Permission%20Checker.js.map
