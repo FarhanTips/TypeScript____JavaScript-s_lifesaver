9f63f52f7f67b3dd3fe0709d1e1075aa0b0254fc
FarhanTips <farhanakbor02@gmail.com>
2026-08-21 16:28:47 +0600

    Product Category Search

--- 4. Problem Solving with TypeScript/dist/6. Permission Checker.js
+++ 4. Problem Solving with TypeScript/dist/6. Permission Checker.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function canEdit(role) {
-    return role == "admin" || role == "editor" ? true : false;
+    return role === "admin" || role === "editor" ? true : false;
 }
 console.log(canEdit("admin"));
 // true 
---
+++ 4. Problem Solving with TypeScript/dist/7. Category Search.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=7.%20Category%20Search.d.ts.map
